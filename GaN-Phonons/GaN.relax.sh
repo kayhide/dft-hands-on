e8a6f44955e71836e7eb31c1db31eaa4cec2ad85
Hideaki Kawai <kayhide@gmail.com>
2023-03-28 07:31:06 +0200

    Fix cutoff energy try various nk

--- GaN-Phonons/GaN.relax.sh
+++ GaN-Phonons/GaN.relax.sh
@@ -2,15 +2,17 @@
 
 module load QuantumESPRESSO/6.8-intel-2021a
 
-NAME="ecut"
+NAME="nk"
 
 echo $NAME
 ####################################################
 
 out=output
 
-for CUTOFF in $(seq 50 50 500); do
-cat > $out/${NAME}_${CUTOFF}.in << EOF
+CUTOFF=200
+
+for NK in $(seq 20 5 100); do
+cat > $out/${NAME}_${NK}.in << EOF
 &CONTROL
     calculation = 'scf',
     verbosity = 'high',
@@ -44,11 +46,11 @@ ATOMIC_POSITIONS crystal
    N  0.3333333333333333    0.6666666666666666    0.3759162900000002
 
 K_POINTS automatic
-    20 20 20 0 0 0
+    ${NK} ${NK} ${NK} 0 0 0
 EOF
 
 
-cat > $out/${NAME}_${CUTOFF}.slrm << EOF
+cat > $out/${NAME}_${NK}.slrm << EOF
 #!/bin/bash
 #SBATCH --partition=batch
 #SBATCH --job-name=MAH
@@ -61,10 +63,10 @@ cat > $out/${NAME}_${CUTOFF}.slrm << EOF
 
 module load QuantumESPRESSO/6.8-intel-2021a
 
-srun pw.x < $out/${NAME}_${CUTOFF}.in > $out/${NAME}_${CUTOFF}.out
+srun pw.x < $out/${NAME}_${NK}.in > $out/${NAME}_${NK}.out
 EOF
 
-sbatch "$out/${NAME}_${CUTOFF}.slrm"
+sbatch "$out/${NAME}_${NK}.slrm"
 
 sleep 1
 
